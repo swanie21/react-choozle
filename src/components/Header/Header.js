276433d7e510332399553103f1ac5519852cd2a6
swanie21 <kswanie21@gmail.com>
2017-09-24 18:06:02 -0600

    add parallax effect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../../images/logo.png';
 import Styles from './Header.scss';
 import Nav from '../Nav/Nav';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className={ Styles.header } role='navigation'>
-        <img className={ Styles.logo } src={ logo } alt='logo' />
-        <Nav />
-      </header>
-    );
-  }
+function Header () {
+  return (
+    <header className={ Styles.header } role='navigation'>
+      <img className={ Styles.logo } src={ logo } alt='logo' />
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Styles from './App.scss';
 import Header from '../../components/Header/Header';
 
-const heroImage = 'https://choozle.com/img/homepage/homepage-hero-bg.jpg';
-
 class App extends Component {
   render() {
     return (
-      <div className={ Styles.app }>
+      <div>
         <Header />
-        <img className={ Styles.hero } src={ heroImage } alt='hero' />
+        <div className={ Styles.heroImage }></div>
+        <div className={ Styles.banner }>
+          <h1 className={ Styles.bannerTitle }>The Fastest Growing Digital Marketing & Advertising Platform</h1>
+        </div>
+        <div className={ Styles.content }></div>
       </div>
     );
   }
